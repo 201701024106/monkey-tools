18926d7480f7de2b2e429c93b574cedc592b4a81
wangds_ext <wds@ebidding.org.cn>
2025-12-27 11:40:55 +0800

    feat: init

---
+++ temp.js
@@ -0,0 +1,13 @@
+// user script
+// @name hello world
+// @namespace   wds_wyl.com
+// @version     1.0.0
+// @description     hello world
+// @author      201647997@qq.com
+// @match       *://*
+// @icon
+// @grant
+(function () {
+  "use strict";
+  console.log("hello world");
+})();
